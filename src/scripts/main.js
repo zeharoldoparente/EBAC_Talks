a8b5093fa9d91f041998cb51dcf5ce1e9cfdb10d
Zé Aroldo <joseharoldoparente@gmail.com>
2024-02-19 16:49:24 -0400

    finish project

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-const dateEvent = new Date("Feb 2, 2024, 19:00:00");
+const dateEvent = new Date("Feb 24, 2024, 19:00:00");
 const timeStampToEvent = dateEvent.getTime();
 
 const countIsHours = setInterval(function () {
@@ -11,12 +11,19 @@ const countIsHours = setInterval(function () {
 
    const dayInMs = 1000 * 60 * 60 * 24;
    const hoursInMs = 1000 * 60 * 60;
-   const daysInMs = 1000 * 60;
+   const minutesInMs = 1000 * 60;
 
-   const daysEvent = Math.floor(distItEvent / (1000 * 60 * 60 * 24));
-   const hoursEvent = Math.floor(
-      (distItEvent % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-   );
-   const minutesEvent =
-      Match.floor(distItEvent % (1000 * 60 * 60)) / (1000 * 60);
+   const daysAtEvent = Math.floor(distItEvent / dayInMs);
+   const hoursAtEvent = Math.floor((distItEvent % dayInMs) / hoursInMs);
+   const minutesAtEvent = Math.floor((distItEvent % hoursInMs) / minutesInMs);
+   const secondsAtEvent = Math.floor((distItEvent % minutesInMs) / 1000);
+
+   document.getElementById(
+      "contador"
+   ).innerHTML = `${daysAtEvent}d ${hoursAtEvent}h ${minutesAtEvent}m ${secondsAtEvent}s`;
+
+   if (distItEvent < 0) {
+      clearInterval(dateEvent);
+      document.getElementById("contador").innerHTML = "O Evento expirou!";
+   }
 }, 1000);
